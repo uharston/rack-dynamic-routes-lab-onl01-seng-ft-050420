5c5655adb67c2c1c8496ae027a0933e7d6bc6294
Uriah Harston <uharston@gmail.com>
2020-06-11 17:19:44 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,11 +9,11 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find {|i| i.name == item_name}
-      if item == nil 
-        resp.status == 400 
-      else 
-        resp.write item.price 
-      end 
+      if item == nil
+        resp.status == 400
+      else
+        resp.write item.price
+      end
     else
       resp.write "Route not found"
       resp.status = 404
