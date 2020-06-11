6b9c10b3872b414968c36e2ac1ddd03c9536e536
Uriah Harston <uharston@gmail.com>
2020-06-11 17:13:10 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      item_name = req.path.split("/items/").last 
+      item_name = req.path.split("/items/").last
       resp write @@items
 
     # elsif req.path.match()
