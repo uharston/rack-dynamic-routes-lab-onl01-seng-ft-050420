64e547ef165420b9081d9dbd6eda41bbf7ef9cbb
Uriah Harston <uharston@gmail.com>
2020-06-11 16:53:29 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,16 +1,16 @@
 class Application
 
   def call(env)
-    resp = Rack::Response.new 
+    resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match()
 
-    else 
+    else
       resp.status "404"
 
 
     resp.finish
   end
 
-end 
+end
