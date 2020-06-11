3235def8335dfc0a8663c44b0252a47bc73e2a7e
Uriah Harston <uharston@gmail.com>
2020-06-11 16:53:25 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,13 @@
 class Application
 
   def call(env)
+    resp = Rack::Response.new 
+    req = Rack::Request.new(env)
+
+    if req.path.match()
+
+    else 
+      resp.status "404"
 
 
     resp.finish
